e71afdd9071d40d9ef32b336a549b6fdb93626a6
Lukas <lukas@amtoft.eu>
2021-11-30 16:36:18 +0100

    running parents caprover

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 mkdir /captain
 mkdir /etc/prometheus
+mkdir /etc/jenkins_home
 cp ./prometheus.yml /etc/prometheus/prometheus.yml
 cp ./caprover-backup.tar /captain/backup.tar
+chmod 777 /var/run/docker.sock
+chmod -R 777 /etc/jenkins_home
+
 docker-compose up
