7be61723508f9628863d155d1705009f8ba530c4
LAD <lukas@amtoft.eu>
2021-12-02 17:25:55 +0100

    Sudo i kommandoerne

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-useradd -u 1201 -G docker jenkins
+sudo useradd -u 1201 -G docker jenkins
 
-mkdir /captain
-mkdir /etc/prometheus
-mkdir /var/jenkins_home
-chown -R jenkins /var/jenkins_home
-cp ./prometheus.yml /etc/prometheus/prometheus.yml
-cp ./caprover-backup.tar /captain/backup.tar
-tar -xzvf jenkins-backup.tar -C /var/jenkins_home
+sudo mkdir /captain
+sudo mkdir /etc/prometheus
+sudo mkdir /var/jenkins_home
+sudo chown -R jenkins /var/jenkins_home
+sudo cp ./prometheus.yml /etc/prometheus/prometheus.yml
+sudo cp ./caprover-backup.tar /captain/backup.tar
+sudo tar -xzvf jenkins-backup.tar -C /var/jenkins_home
 
-docker-compose up
+docker-compose up -d
