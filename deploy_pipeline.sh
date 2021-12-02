243193e5779c17533f5930c030076363673e51d4
LAD <lukas@amtoft.eu>
2021-12-02 21:29:28 +0100

    Update to include docker login from outside

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo useradd -u 1201 -G docker jenkins
+sudo useradd -u 1201 -G docker,$USER jenkins
 
 sudo mkdir /captain
 sudo mkdir /etc/prometheus
 sudo mkdir /var/jenkins_home
 
 sudo chown -R jenkins /var/jenkins_home
+sudo chmod 760 ~/.docker
+sudo chmod 660 ~/.docker/config.json
 
 sudo cp ./prometheus.yml /etc/prometheus/prometheus.yml
 sudo cp ./caprover-backup.tar /captain/backup.tar
