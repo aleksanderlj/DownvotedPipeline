fd62c85b5f916d67919d38b8e06ecfedc7872a13
LAD <lukas@amtoft.eu>
2021-12-02 16:25:29 +0100

    Update to add jenkins. But jenkins_backup is too large

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -7,5 +7,6 @@ mkdir /var/jenkins_home
 chown -R jenkins /var/jenkins_home
 cp ./prometheus.yml /etc/prometheus/prometheus.yml
 cp ./caprover-backup.tar /captain/backup.tar
+tar -xzvf jenkins-backup.tar -C /var/jenkins_home
 
 docker-compose up
